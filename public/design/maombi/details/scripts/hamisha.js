9f8f66bbcd0ed2ccf72cf055ab3b1c39943ea133
mape.ally <mape.ally@gmail.com>
2023-09-10 15:44:27 +0300

    clothing menu

--- public/design/maombi/details/scripts/hamisha.js
+++ public/design/maombi/details/scripts/hamisha.js
@@ -4,17 +4,16 @@ function wasilisha(){
     var userLevel = document.getElementById('userLevel').value;
     var haliombi = 1;
     var trackerId = document.getElementById('trackerId').value;
-
     var staffsInput = document.getElementById('staffs').value;
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
-            "haliombi": haliombi, "trackerId": trackerId, 
-            "attachment": "", "kiambatisho": "", 
-            "attach_length": ""}),
+            "haliombi": haliombi, "trackerId": trackerId, "oldstream": "",
+            "attachment": "", "kiambatisho": "", "newstream": "",
+            "attach_length": "", "establishId": ""}),
             contentType: 'application/json',
             success: function(response) {
                 window.location.href = "/MaombiKuanzishaShule";
@@ -42,7 +41,7 @@ function rudisha(){
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
@@ -70,45 +69,11 @@ function wasilishak1(){
     var userLevel = document.getElementById('userLevel').value;
     var haliombi = 1;
     var trackerId = document.getElementById('trackerId').value;
-
     var staffsInput = document.getElementById('staffs').value;
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
-            type: 'POST',
-            data: JSON.stringify({"coments": coments, "staffs": staffs, 
-            "haliombi": haliombi, "trackerId": trackerId, 
-            "attachment": "", "kiambatisho": "", 
-            "attach_length": ""}),
-            contentType: 'application/json',
-            success: function(response) {
-                window.location.href = "/MaombiKuanzishaShule";
-            }
-        });
-    }else{
-        alert("Samahani huwezi kubonyeza kitufe hiki")
-    }
-    }else{
-    alert("Tafadhali weka maoni yako")
-    }
-
-}
-
-function wasilishakAdsa(){
-    // var fileInput = document.querySelector('#formFileMultiple');
-    // if(document.getElementById("formFileMultiple").files.length <= 0){
-    var coments = document.getElementById('exampleFormControlTextarea1').value;
-    var staffs = '0-9';
-    var userLevel = document.getElementById('userLevel').value;
-    var haliombi = 1;
-    var trackerId = document.getElementById('trackerId').value;
-
-    var staffsInput = document.getElementById('staffs').value;
-    if(coments.length > 0){
-        if(staffsInput == '#'){
-        $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
@@ -135,19 +100,24 @@ function wasilishaKe(){
     var haliombi = 2;
     
     var trackerId = document.getElementById('trackerId').value;
-    var schoolId = document.getElementById('schoolId').value;
+    var newstream = document.getElementById('WardIdNew').value;
+    var oldstream = document.getElementById('WardIdOld').value;
+    var establishId = document.getElementById('establishId').value;
+
     var staffsInput = document.getElementById('staffs').value;
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
-            "attachment": "", "kiambatisho": "", "schoolId": schoolId,
-            "attach_length": ""}),
+            "attachment": "", "kiambatisho": "", "establishId": establishId,
+            "attach_length": "", "oldstream": oldstream, "newstream": newstream,
+        }),
             contentType: 'application/json',
             success: function(response) {
+                alert("Shule imehamishwa kikamilifu")
                 window.location.href = "/MaombiKuanzishaShule";
             }
         });
@@ -167,13 +137,11 @@ function kataaKe(){
     var haliombi = 3;
     
     var trackerId = document.getElementById('trackerId').value;
-    
-    var schoolCategoryID = document.getElementById('schoolCategoryID').value;
     var staffsInput = document.getElementById('staffs').value;
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
@@ -181,6 +149,7 @@ function kataaKe(){
             "attach_length": ""}),
             contentType: 'application/json',
             success: function(response) {
+                alert("Ombi la kubadili Mkondo limekataliwa")
                 window.location.href = "/MaombiKuanzishaShule";
             }
         });
@@ -194,7 +163,7 @@ function kataaKe(){
 }
 
 function tuma(){
-    var gamechanger = document.getElementById('gamechanger').value;
+        var gamechanger = document.getElementById('gamechanger').value;
         var coments = document.getElementById('exampleFormControlTextarea1').value;
         var userLevel = document.getElementById('userLevel').value;
         var staffs = document.getElementById('staffs').value;
@@ -202,7 +171,7 @@ function tuma(){
         var trackerId = document.getElementById('trackerId').value;
         if(coments.length > 0){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
@@ -218,58 +187,7 @@ function tuma(){
         }
 }
 
-function nata(){
-    $.ajax({
-        url: "/MaombiKuanzishaShuleList",
-        type: 'GET',
-        contentType: 'application/json',
-        success: function(response) {
-            if(typeof(response) === "string"){response = JSON.parse(response)}
-            $("#tasksTable").find('tbody').empty();
-            for (var i=0; i < response.length; i++) {
-                var row = '<tr> <th scope="row">'+
-                                '<div class="form-check">'+
-                                    '<input class="form-check-input" type="checkbox" name="chk_child" value="option1">'+
-                                '</div>'+
-                            '</th>'; 
-                row = row + '<td class="id" id="tracker"><a href="TaarifaOmbi/'+response[i].tracking_number+'" class="fw-medium link-primary"> ' + response[i].tracking_number + ' </a></td>';
-                row = row + '<td class="project_name"><a href="apps-projects-overview.html" class="fw-medium link-primary"> ' + response[i].school_name + ' </a></td>';
-                row = row + '<td>'+
-                            '<div class="d-flex">'+
-                                '<div class="flex-grow-1 tasks_name"> ' + response[i].LgaName + ' </div>'+
-                                    '<div class="flex-shrink-0 ms-4">'+
-                                '<ul class="list-inline tasks-list-menu mb-0">'+
-                                '<li class="list-inline-item"><a href="apps-tasks-details.html"><i class="ri-eye-fill align-bottom me-2 text-muted"></i></a></li>'+
-                                    '<li class="list-inline-item"><a class="edit-item-btn" href="#showModal" data-bs-toggle="modal"><i class="ri-pencil-fill align-bottom me-2 text-muted"></i></a></li>'+
-                                '<li class="list-inline-item">'+
-                            '<a class="remove-item-btn" data-bs-toggle="modal" href="#deleteOrder">'+
-                            '<i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>'+
-                            '</a>'+
-                            '</li>'+
-                            '</ul>'+
-                            '</div>'+
-                            '</div>'+
-                            '</td>';
-                row = row + '<td class="status"><span class=""> ' + response[i].RegionName + ' </span></td>';
-                row = row + ' <td class="due_date badge badge-soft-secondary text-uppercase"  title=" ' + response[i].created_at + ' "> ' + response[i].remain_days + ' </td>';
-                row = row + '<td class="priority"><span class="badge bg-danger text-uppercase">High</span></td>';
-                row = row + '<td class="assignedto">'+
-                            '<div class="avatar-group">'+
-                                '<a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Frank">'+
-                                    '<img src="design/assets/images/users/avatar-3.jpg" alt="" class="rounded-circle avatar-xxs" />'+
-                                '</a>'+
-                                '<a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Anna">'+
-                                    '<img src="design/assets/images/users/avatar-1.jpg" alt="" class="rounded-circle avatar-xxs" />'+
-                                '</a>'+
-                            '</div>'+
-                            '</td>'+
-                            '</tr>';
-                $('#tasksTable').append(row);
-            }
-        }
-    });
-}
-window.onload = nata;
+
 
 $('a').click(function(){
     var base64doc = $(this).attr('data-path');
@@ -398,12 +316,11 @@ function rudishaMteja(){
     var userLevel = document.getElementById('userLevel').value;
     var haliombi = 4;
     var trackerId = document.getElementById('trackerId').value;
-
     var staffsInput = document.getElementById('staffs').value;
     if(coments.length > 0){
         if(staffsInput == '#'){
         $.ajax({
-            url: "/FutaComment",
+            url: "/HamishaComment",
             type: 'POST',
             data: JSON.stringify({"coments": coments, "staffs": staffs, 
             "haliombi": haliombi, "trackerId": trackerId, 
@@ -421,4 +338,132 @@ function rudishaMteja(){
     alert("Tafadhali weka maoni yako")
     }
 
+}
+
+function wasilishaMus(){
+    var coments = document.getElementById('exampleFormControlTextarea1').value;
+    var staffs = '0-9';
+    var userLevel = document.getElementById('userLevel').value;
+    var haliombi = 1;
+    
+    var trackerId = document.getElementById('trackerId').value;
+    
+    var staffsInput = document.getElementById('staffs').value;
+    if(coments.length > 0){
+        if(staffsInput == '#'){
+    $.ajax({
+        url: "/HamishaComment",
+        type: 'POST',
+        data: JSON.stringify({"coments": coments, "staffs": staffs,
+        "haliombi": haliombi, "trackerId": trackerId,
+        "attachment": "", "kiambatisho": "", 
+        "attach_length": 0, "ombitype": 1}),
+        contentType: 'application/json',
+        success: function(response) {
+            window.location.href = "/MaombiKusajiliShule";
+        }
+    });
+}else{
+    alert("Samahani huwezi kubonyeza kitufe hiki")
+}
+    }else{
+    alert("Tafadhali weka maoni yako")
+    }
+
+}
+
+function wasilishaAdsa(){
+    var coments = document.getElementById('exampleFormControlTextarea1').value;
+    var staffs = '0-9';
+    var userLevel = document.getElementById('userLevel').value;
+    var haliombi = 1;
+    
+    var trackerId = document.getElementById('trackerId').value;
+    
+    var staffsInput = document.getElementById('staffs').value;
+    if(coments.length > 0){
+        if(staffsInput == '#'){
+    $.ajax({
+        url: "/HamishaComment",
+        type: 'POST',
+        data: JSON.stringify({"coments": coments, "staffs": staffs,
+        "haliombi": haliombi, "trackerId": trackerId,
+        "attachment": "", "kiambatisho": "", 
+        "attach_length": 0, "ombitype": 1}),
+        contentType: 'application/json',
+        success: function(response) {
+            window.location.href = "/MaombiKusajiliShule";
+        }
+    });
+}else{
+    alert("Samahani huwezi kubonyeza kitufe hiki")
+}
+    }else{
+    alert("Tafadhali weka maoni yako")
+    }
+
+}
+
+function rudishaMus(){
+    // var fileInput = document.querySelector('#formFileMultiple');
+    // if(document.getElementById("formFileMultiple").files.length <= 0){
+    var coments = document.getElementById('exampleFormControlTextarea1').value;
+    var staffs = '0-2';
+    var userLevel = document.getElementById('userLevel').value;
+    var haliombi = 1;
+    
+    var trackerId = document.getElementById('trackerId').value;
+    
+    var staffsInput = document.getElementById('staffs').value;
+    if(coments.length > 0){
+        if(staffsInput == '#'){
+    $.ajax({
+        url: "/HamishaComment",
+        type: 'POST',
+        data: JSON.stringify({"coments": coments, "staffs": staffs,
+        "haliombi": haliombi, "trackerId": trackerId,
+        "attachment": "", "kiambatisho": "", 
+        "attach_length": 0, "ombitype": 1}),
+        contentType: 'application/json',
+        success: function(response) {
+            window.location.href = "/MaombiKusajiliShule";
+        }
+    });
+}else{
+    alert("Samahani huwezi kubonyeza kitufe hiki")
+}
+    }else{
+    alert("Tafadhali weka maoni yako")
+    }
+
+}
+
+function wasilishaMmus(){
+    var coments = document.getElementById('exampleFormControlTextarea1').value;
+    var staffs = '0-12';
+    var userLevel = document.getElementById('userLevel').value;
+    var haliombi = 1;
+    var trackerId = document.getElementById('trackerId').value;
+    
+    var staffsInput = document.getElementById('staffs').value;
+    if(coments.length > 0){
+        if(staffsInput == '#'){
+    $.ajax({
+        url: "/HamishaComment",
+        type: 'POST',
+        data: JSON.stringify({"coments": coments, "staffs": staffs,
+        "haliombi": haliombi, "trackerId": trackerId,
+        "attachment": "", "kiambatisho": "", 
+        "attach_length": 0, "ombitype": 1}),
+        contentType: 'application/json',
+        success: function(response) {
+            window.location.href = "/MaombiKusajiliShule";
+        }
+    });
+}else{
+    alert("Samahani huwezi kubonyeza kitufe hiki")
+}
+    }else{
+    alert("Tafadhali weka maoni yako")
+    }
 }
