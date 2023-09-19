4cf7839b7fc8711d8303f38e506b0e0b81d6c94c
mape.ally <mape.ally@gmail.com>
2023-09-19 10:51:01 +0300

    Maombi kuanzisha

--- irs_system.js
+++ irs_system.js
@@ -4616,130 +4616,130 @@ app.post("/sajiliHati", function (req, res) {
 
 
 
-app.get("/TaarifaOmbi/:id", function (req, res) {
-  var obj = [];
-  var TrackingNumber = req.params.id;
-  if (
-    typeof req.session.userName !== "undefined" ||
-    req.session.userName === true
-  ) {
-    request(
-      {
-        url: ombiDetails,
-        method: "POST",
-        headers: {
-          Authorization: "Bearer" + " " + req.session.Token,
-          "Content-Type": "application/json",
-        },
-        json: {
-          browser_used: req.session.browser_used,
-          ip_address: req.session.ip_address,
-          TrackingNumber: TrackingNumber,
-          userLevel: req.session.UserLevel,
-          office: req.session.office,
-        },
-      },
-      function (error, response, body) {
-        if (error) {
-          console.log(
-            new Date() + ": fail to MaombiKuanzishaShuleJumla " + error
-          );
-          res.send("failed");
-        }
+// app.get("/TaarifaOmbi/:id", function (req, res) {
+//   var obj = [];
+//   var TrackingNumber = req.params.id;
+//   if (
+//     typeof req.session.userName !== "undefined" ||
+//     req.session.userName === true
+//   ) {
+//     request(
+//       {
+//         url: ombiDetails,
+//         method: "POST",
+//         headers: {
+//           Authorization: "Bearer" + " " + req.session.Token,
+//           "Content-Type": "application/json",
+//         },
+//         json: {
+//           browser_used: req.session.browser_used,
+//           ip_address: req.session.ip_address,
+//           TrackingNumber: TrackingNumber,
+//           userLevel: req.session.UserLevel,
+//           office: req.session.office,
+//         },
+//       },
+//       function (error, response, body) {
+//         if (error) {
+//           console.log(
+//             new Date() + ": fail to MaombiKuanzishaShuleJumla " + error
+//           );
+//           res.send("failed");
+//         }
 
-        if (body !== undefined) {
-          // var jsonData = JSON.parse(body)
-          var jsonData = body;
+//         if (body !== undefined) {
+//           // var jsonData = JSON.parse(body)
+//           var jsonData = body;
 
-          // console.log(jsonData)
-          var message = jsonData.message;
-          var statusCode = jsonData.statusCode;
-          var data = jsonData.data;
-          if (statusCode == 300) {
-            var remain_days = data[0].remain_days;
-            var created_at = data[0].created_at;
-            var tracking_number = data[0].tracking_number;
-            var school_name = data[0].school_name;
-            var LgaName = data[0].LgaName;
-            var registryTypeId = data[0].registry_type_id;
-            var WardNameMtu = data[0].WardNameMtu;
-            var LgaNameMtu = data[0].LgaNameMtu;
-            var RegionName = data[0].RegionName;
-            var RegionNameMtu = data[0].RegionNameMtu;
-            var fullname = data[0].fullname;
-            var schoolCategory = data[0].schoolCategory;
-            var registry = data[0].registry;
-            var occupation = data[0].occupation;
-            var mwombajiAddress = data[0].mwombajiAddress;
-            var mwombajiPhoneNo = data[0].mwombajiPhoneNo;
-            var baruaPepe = data[0].baruaPepe;
-            var language = data[0].language;
-            var school_size = data[0].school_size;
-            var area = data[0].area;
-            var WardName = data[0].WardName;
-            var structure = data[0].structure;
-            var subcategory = data[0].subcategory;
-            var count = jsonData.maoni[0].count;
-            var objAttachment = jsonData.objAttachment;
-            var objAttachment1 = jsonData.objAttachment1;
-            var objAttachment2 = jsonData.objAttachment2;
-            var Maoni = jsonData.Maoni;
-            console.log(
-              new Date() + " " + req.session.userName + ": /TaarifaOmbi"
-            );
-            res.render(
-              path.join(__dirname + "/public/design/maombi/details/view-ombi-details"),
-              {
-                req: req,
-                muda_ombi: remain_days,
-                useLev: req.session.UserLevel,
-                                  userName: req.session.userName,
-              RoleManage: req.session.RoleManage,
-    userID: req.session.userID,
-                cheoName: req.session.cheoName,
-                created_at: created_at,
-                tracking_number: tracking_number,
-                school_name: school_name,
-                LgaName: LgaName,
-                RegionName: RegionName,
-                RegionNameMtu: RegionNameMtu,
-                fullname: fullname,
-                schoolCategory: schoolCategory,
-                registry: registry,
-                occupation: occupation,
-                mwombajiAddress: mwombajiAddress,
-                registryTypeId: registryTypeId,
-                mwombajiPhoneNo: mwombajiPhoneNo,
-                baruaPepe: baruaPepe,
-                language: language,
-                school_size: school_size,
-                userLevel: req.session.UserLevel,
-                area: area,
-                WardName: WardName,
-                structure: structure,
-                LgaNameMtu: LgaNameMtu,
-                WardNameMtu: WardNameMtu,
-                subcategory: subcategory,
-                count: count,
-                staffs: jsonData.staffs,
-                status: jsonData.status,
-                objAttachment: objAttachment,
-                objAttachment1: objAttachment1,
-                Maoni: Maoni,
-                objAttachment2: objAttachment2,
-              }
-            );
-          }
-          if (statusCode == 209) {
-            res.redirect("/");
-          }
-        }
-      }
-    );
-  } else {
-    res.redirect("/");
-  }
-});
+//           // console.log(jsonData)
+//           var message = jsonData.message;
+//           var statusCode = jsonData.statusCode;
+//           var data = jsonData.data;
+//           if (statusCode == 300) {
+//             var remain_days = data[0].remain_days;
+//             var created_at = data[0].created_at;
+//             var tracking_number = data[0].tracking_number;
+//             var school_name = data[0].school_name;
+//             var LgaName = data[0].LgaName;
+//             var registryTypeId = data[0].registry_type_id;
+//             var WardNameMtu = data[0].WardNameMtu;
+//             var LgaNameMtu = data[0].LgaNameMtu;
+//             var RegionName = data[0].RegionName;
+//             var RegionNameMtu = data[0].RegionNameMtu;
+//             var fullname = data[0].fullname;
+//             var schoolCategory = data[0].schoolCategory;
+//             var registry = data[0].registry;
+//             var occupation = data[0].occupation;
+//             var mwombajiAddress = data[0].mwombajiAddress;
+//             var mwombajiPhoneNo = data[0].mwombajiPhoneNo;
+//             var baruaPepe = data[0].baruaPepe;
+//             var language = data[0].language;
+//             var school_size = data[0].school_size;
+//             var area = data[0].area;
+//             var WardName = data[0].WardName;
+//             var structure = data[0].structure;
+//             var subcategory = data[0].subcategory;
+//             var count = jsonData.maoni[0].count;
+//             var objAttachment = jsonData.objAttachment;
+//             var objAttachment1 = jsonData.objAttachment1;
+//             var objAttachment2 = jsonData.objAttachment2;
+//             var Maoni = jsonData.Maoni;
+//             console.log(
+//               new Date() + " " + req.session.userName + ": /TaarifaOmbi"
+//             );
+//             res.render(
+//               path.join(__dirname + "/public/design/maombi/details/view-ombi-details"),
+//               {
+//                 req: req,
+//                 muda_ombi: remain_days,
+//                 useLev: req.session.UserLevel,
+//                                   userName: req.session.userName,
+//               RoleManage: req.session.RoleManage,
+//     userID: req.session.userID,
+//                 cheoName: req.session.cheoName,
+//                 created_at: created_at,
+//                 tracking_number: tracking_number,
+//                 school_name: school_name,
+//                 LgaName: LgaName,
+//                 RegionName: RegionName,
+//                 RegionNameMtu: RegionNameMtu,
+//                 fullname: fullname,
+//                 schoolCategory: schoolCategory,
+//                 registry: registry,
+//                 occupation: occupation,
+//                 mwombajiAddress: mwombajiAddress,
+//                 registryTypeId: registryTypeId,
+//                 mwombajiPhoneNo: mwombajiPhoneNo,
+//                 baruaPepe: baruaPepe,
+//                 language: language,
+//                 school_size: school_size,
+//                 userLevel: req.session.UserLevel,
+//                 area: area,
+//                 WardName: WardName,
+//                 structure: structure,
+//                 LgaNameMtu: LgaNameMtu,
+//                 WardNameMtu: WardNameMtu,
+//                 subcategory: subcategory,
+//                 count: count,
+//                 staffs: jsonData.staffs,
+//                 status: jsonData.status,
+//                 objAttachment: objAttachment,
+//                 objAttachment1: objAttachment1,
+//                 Maoni: Maoni,
+//                 objAttachment2: objAttachment2,
+//               }
+//             );
+//           }
+//           if (statusCode == 209) {
+//             res.redirect("/");
+//           }
+//         }
+//       }
+//     );
+//   } else {
+//     res.redirect("/");
+//   }
+// });
 
 app.get("/FutaShuleTaarifa/:id", function (req, res) {
   var obj = [];
