2890ad4150b1b12f54a887f652e6b3fcc1975ed8
mape.ally <mape.ally@gmail.com>
2024-02-19 14:27:43 +0300

    barua

--- public/controllers/barua/baruaController.js
+++ public/controllers/barua/baruaController.js
@@ -17,7 +17,6 @@ baruaController.get("/barua/:tracking_number", cors(), isAuthenticated,can('view
         // console.log(type)
         sendRequest(req , res , baruaDetailsAPI +"/"+ tracking_number , 'POST' , formData , (jsonData) => {
           const { statusCode, data, sqa_zone_region } = jsonData;
-          // console.log(data)
           if(statusCode == 300){
             const {
               school_name,
