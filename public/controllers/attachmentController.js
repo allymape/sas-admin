22af81eba15bde8624cd786799ce5782e6b7cb65
mape.ally <mape.ally@gmail.com>
2024-08-20 15:02:45 +0300

    handle ssl view attachments

--- public/controllers/attachmentController.js
+++ public/controllers/attachmentController.js
@@ -9,11 +9,36 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const pdf2base64 = require("pdf-to-base64");
 var API_BASE_URL = process.env.API_BASE_URL;
 var pandishaHatiAPI = API_BASE_URL + "upload-attachment";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const https = require("https");
+const fs = require("fs");
+const axios = require("axios");
+// Custom HTTPS agent with SSL verification
+const agent = new https.Agent({
+  ca: fs.readFileSync("/etc/ssl/certs/ca-certificates.crt"),
+  rejectUnauthorized: true, // Ensures SSL verification
+});
 
+async function pdfbase64(url) {
+  try {
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+      httpsAgent: agent,
+    });
+    const base64 = Buffer.from(response.data).toString("base64");
+    return base64;
+  } catch (error) {
+    console.error(
+      "Error downloading or converting PDF to base64:",
+      error.message
+    );
+    throw error;
+  }
+}
 attachmentController.post("/View-Attachment", isAuthenticated, function (req, res) {
     const file_path = req.body.file_path;
     if(file_path){
-        pdf2base64(FRONTEND_URL + file_path)
+        pdfbase64('https://accredit.moe.go.tz:8009/attachments/20240820114852HbHtPxuTf5.pdf')
         .then( (response) => {
                 // console.log(response);
                 res.send({
